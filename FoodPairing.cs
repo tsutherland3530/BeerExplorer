e5d12f5eb054cb55718207fc8ad89b82e03d22ab
Tommy Sutherland <tsutherland3530@gmail.com>
2024-08-04 09:41:01 -0400

    updated foodpairing.cs
    updated file for just IPAs

---
+++ FoodPairing.cs
@@ -0,0 +1,9 @@
+namespace BeerExplorer.Models
+{
+    public class FoodPairing
+    {
+        public int Id { get; set; }
+        public string Food { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
