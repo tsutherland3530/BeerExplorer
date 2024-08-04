5adba27a0d37109520cd36d785c639ea4d6e20f9
Tommy Sutherland <tsutherland3530@gmail.com>
2024-08-04 09:35:56 -0400

    created tests for the BeerStyles.cs
    made a few unit tests for the app

---
+++ BeerStyleTests.cs/BeerStyleTests.cs
@@ -0,0 +1,38 @@
+﻿using BeerExplorer.Controllers;
+using BeerExplorer.Data;
+using BeerExplorer.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace BeerExplorer.Tests
+{
+    public class BeerStyleTests
+    {
+        [Fact]
+        public void CanAddBeerStyle()
+        {
+            var options = new DbContextOptionsBuilder<BeerContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new BeerContext(options))
+            {
+                var controller = new BeerStylesController(context);
+                var beerStyle = new BeerStyle
+                {
+                    Name = "Test IPA",
+                    Description = "A test IPA beer.",
+                    OriginStory = "Test Origin",
+                    ABV = "6.5%"
+                };
+
+                controller.Create(beerStyle);
+
+                Assert.Single(context.BeerStyles);
+                Assert.Equal("Test IPA", context.BeerStyles.First().Name);
+            }
+        }
+
+        // Add more unit tests as needed
+    }
+}
