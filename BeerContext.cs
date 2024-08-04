ad2aae2b0bdc4c5e33b951eae23f364a0966c2b7
Tommy Sutherland <tsutherland3530@gmail.com>
2024-08-04 09:39:32 -0400

    changed context.cs
    made changes to match updated DbInitializer

---
+++ BeerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BeerExplorer.Models;
+
+namespace BeerExplorer.Data
+{
+    public class BeerContext : DbContext
+    {
+        public BeerContext(DbContextOptions<BeerContext> options) : base(options) { }
+
+        public DbSet<BeerStyle> BeerStyles { get; set; }
+        public DbSet<FoodPairing> FoodPairings { get; set; }
+    }
+}
